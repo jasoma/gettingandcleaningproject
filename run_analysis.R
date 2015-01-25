094697afca6958df8dddae1374a6986fe40e8a2c
Jason Maher <jason@jasonmaher.me>
2015-01-25 11:55:25 +0900

    Can load the y_ and subject_ files as well

--- run_analysis.R
+++ run_analysis.R
@@ -30,7 +30,7 @@ features.all <- read.table("data/features.txt", colClasses = c("numeric", "chara
 features.mean_std <- grep("-mean|-std", features.all$label, value = TRUE)
 features <- features.all[features.all$label %in% features.mean_std, ]
 
-# when loading the 'X' data files we want to discard the columns that are not a mean or std column.
+# when loading the 'X_' data files we want to discard the columns that are not a mean or std column.
 # to do that we construct a character vector to pass to colClasses where columns we want have a class
 # of 'numeric' and columns we don't have 'NULL' which will cause `read.table` to discard them.
 is.mean_std <- function(index) {
@@ -40,7 +40,7 @@ is.mean_std <- function(index) {
 data.cols <- sapply(seq(1, max(features.all$index)), function(x) if(is.mean_std(x)) "numeric" else "NULL")
 
 # now we can read in either of the two data files
-read.datafile <- function(filename, ...) {
+read.xfile <- function(filename, ...) {
     # read the file passing in the classes vector we assembled
     df <- read.table(filename, colClasses = data.cols, ...)
     # set the column names from the features data
@@ -48,4 +48,18 @@ read.datafile <- function(filename, ...) {
     df
 }
 
+# get the listing of activity code to activity name
+activitylabels <- read.table("data/activity_labels.txt", colClasses = c("numeric", "character"), col.names = c("code", "label"))
 
+# reading the 'y_' files is much easier, it only contains one column which is the activity factor
+read.yfile <- function(filename) {
+    df <- read.table(filename, colClasses = "factor", col.names = "activity")
+    # set the factor names based on the 'activity_labels.txt' content
+    levels(df$activity) <- activitylabels$label
+    df
+}
+
+# likewise the subject files is easy, no need to even play with factor naming
+read.subjectfile <- function(filename) {
+    read.table(filename, colClasses = "numeric", col.names = "subject_id")
+}
